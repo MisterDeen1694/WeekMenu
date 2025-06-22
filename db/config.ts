5ca1cea5b4311a7d837787aa1afd1d40a49a4767
Brandon <jason-deen@outlook.com>
2025-06-22 18:18:19 +0200

    🗃️ Setup the database

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,43 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from "astro:db";
+
+const Recipe = defineTable({
+	columns: {
+		id: column.text({ primaryKey: true }),
+		title: column.text(),
+		author: column.text(),
+		source: column.text(),
+		cover: column.text(),
+	},
+});
+
+const Product = defineTable({
+	columns: {
+		id: column.text({ primaryKey: true }),
+		name: column.text(),
+		measureUnits: column.text(),
+	},
+});
+
+const RecipeProducts = defineTable({
+	columns: {
+		id: column.text({ primaryKey: true }),
+		recpieId: column.text(),
+		productId: column.text(),
+		amount: column.number(),
+	},
+	foreignKeys: [
+		{
+			columns: ["recpieId"],
+			references: () => [Recipe.columns.id],
+		},
+		{
+			columns: ["productId"],
+			references: () => [Product.columns.id],
+		},
+	],
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+	tables: { Recipe, Product, RecipeProducts },
 });
--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,30 @@
-import { db } from 'astro:db';
+import { db, Product, Recipe, RecipeProducts } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+	await db.insert(Product).values([
+		{
+			id: "knoflook",
+			name: "Knoflook",
+			measureUnits: "teen",
+		},
+	]);
+
+	await db.insert(Recipe).values([
+		{
+			id: "soep",
+			title: "Knoflook Soep",
+			author: "Jason",
+			source: "https://action.com/",
+			cover: "https://i.ytimg.com/an_webp/vTht0OygLWA/mqdefault_6s.webp?du=3000&sqp=CIDI08EG&rs=AOn4CLBuOvZEppjDKjPnGfMqxJKa-nbt2g",
+		},
+	]);
+
+	await db.insert(RecipeProducts).values([
+		{
+			productId: "knoflook",
+			recpieId: "soep",
+			amount: 6,
+		},
+	]);
 }
